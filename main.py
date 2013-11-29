e898b8d0d3e9980b6ae3ac1b33128fabe5165f6c
poku1010 <poku1010@gmail.com>
2013-11-29 16:33:33 +0800

    test

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('PTT Buying Parser GAE.')
+        self.response.write('PTT Buying Parser GAE. test')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
