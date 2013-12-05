c422ed59f5d1cba6669fa2d3063ba76e5e7984f3
Clifford-Yen <masasaboy@yahoo.com.tw>
2013-12-05 20:42:51 +0800

    finish price cut

--- main.py
+++ main.py
@@ -15,10 +15,10 @@
 # limitations under the License.
 #
 
-from google.appengine.ext import ndb
+from google.appengine.ext import ndb # from GAE
 from HTMLParser import HTMLParser
 
-import webapp2
+import webapp2 # from GAE
 import json
 import urllib2
 
@@ -32,30 +32,40 @@ class Item(ndb.Model):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	url='http://pipes.yahoo.com/pipes/pipe.run?_id=29604671a0cf78378f569387a5100e39&_render=json'
-    	response = urllib2.urlopen(url)
+        url='http://pipes.yahoo.com/pipes/pipe.run?_id=29604671a0cf78378f569387a5100e39&_render=json'
+        response = urllib2.urlopen(url)
         data = response.read()
         json_data = json.loads(data)
         items = json_data.get('value')['items']
         
         for item in items:
-		    item_author_name = item.get('author')['name']
-		    #author_name = author.get('name')
-		    item_title = item.get('title')
-		    item_link = item.get('link')
+            item_author_name = item.get('author')['name']
+            #author_name = author.get('name')
+            item_title = item.get('title')
+            item_link = item.get('link')
 
-		    item_description = item.get('description')['content']
-		    item_description_strip = strip_tags(item_description)
+            item_description = item.get('description')['content']
+            item_description_strip = strip_tags(item_description)
 
-		    item_price_start = item_description_strip.find(u'售價格：')
-		    price_start = item_price_start+len(u'售價格：')
-		    item_price_end = item_description_strip[price_start].find(u' ')
-		    item_price = item_description_strip[price_start:]
+            item_price_start = item_description_strip.find(u'欲售價格：')
+            price_start = item_price_start+len(u'欲售價格：')
+            
+            possible_end_words = [u'售出原因', u'交易方式']
+            item_price_end = -1
+            for word in possible_end_words:
+                item_price_end = item_description_strip[price_start:].find(word)
+                if item_price_end != -1: break
+            
+            # item_price_end = item_description_strip[price_start:].find(u'售出原因')
+            # if item_price_end == -1:
+                # item_price_end = item_description_strip[price_start:].find(u'交易方式')
+            
+            item_price = item_description_strip[price_start:price_start+item_price_end]
 
-		    self.response.out.write(item_price)
-		    self.response.out.write('<br><hr>')
-		    
-		    #campaignID = item.get("CampaignID")
+            self.response.out.write(item_price)
+            self.response.out.write('<br><hr>')
+            
+            #campaignID = item.get("CampaignID")
 
         #self.response.write('PTT Buying Parser GAE.')
         #self.response.out.write(json_data)
@@ -73,7 +83,7 @@ def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
- 		
+         
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
