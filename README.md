c59462f249371aa8b33250b4bc950b5efe6be2c5
poku1010 <poku1010@gmail.com>
2014-01-24 14:17:32 +0800

    complete the search mechanism

--- README.md
+++ README.md
@@ -15,17 +15,16 @@ Metadata browsing:
 Object Retrieval:
 =================
 
-* GET http://fleebuy.appspot.com/rest/Item
-** (gets the first N Item instances)
+* Gets the first N Item instances
+GET http://fleebuy.appspot.com/rest/Item
 
-* GET http://fleebuy.appspot.com/rest/Item?offset=10
-** (gets the next N Item instances, starting at offset 10.  Note
+*  Gets the next N Item instances, starting at offset 10.  Note
    the returned list element contains an "offset" attribute.  If it has a
    value, that is the next offset to use to retrieve more results.  If it is
-   empty, there are no more results).
+   empty, there are no more results.
+GET http://fleebuy.appspot.com/rest/Item?offset=10
 
-* GET http://fleebuy.appspot.com/rest/Item?feq_datetime=2014-01-12T08:07:09.200150
-** (gets the Item instances, starting with a date property "equal to" 2014-01-12T08:07:09.200150.
+* Gets the Item instances, starting with a date property "equal to" 2014-01-12T08:07:09.200150.
    Available operators:
      - "feq_" -> "equal to"
      - "flt_" -> "less than"
@@ -35,7 +34,20 @@ Object Retrieval:
      - "fne_" -> "not equal to"
      - "fin_" -> "in <comma_separated_list>"
    Multiple operators may be provided, they are AND'ed together).
+GET http://fleebuy.appspot.com/rest/Item?feq_datetime=2014-01-12T08:07:09.200150
 
-* GET http://fleebuy.appspot.com/rest/Item/aglzfmZsZWVidXlyEQsSBEl0ZW0YgICAgMD6kwsM
-** (gets a single WholeEnchilada instance)
+*Gets a single WholeEnchilada instance
+GET http://fleebuy.appspot.com/rest/Item/aglzfmZsZWVidXlyEQsSBEl0ZW0YgICAgMD6kwsM
 
+Search Function:
+=================
+
+*Normal Usage
+   Available operators:
+     - "is_union" -> must to set the value as true or false
+     - "is_debug" -> set the value as true or false to open the debug mode or not.
+     - "k1" -> keyword sets must follow the sequence 1 -> 2 -> 3
+     - "k2" -> keyword sets
+     - "k3" -> keyword sets
+     
+GET http://fleebuy.appspot.com/search?is_union=false&k1=htc&k2=new
--- main.py
+++ main.py
@@ -20,7 +20,8 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 import models
 from google.appengine.api import search 
-_INDEX_NAME_ALL = 'search_all'
+_INDEX_NAME_ALL = 'test'
+_IS_DEBUG = False
     
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -52,6 +53,7 @@ class MainHandler(webapp.RequestHandler):
             item_link = item.get('link')
 
 			# ===== get price from item description =====
+			     #TODO: need to save log at item_description = item.get('description')['content']
             item_description = item.get('description')['content']
             item_description_strip = strip_tags(item_description)
 
@@ -87,23 +89,25 @@ class MainHandler(webapp.RequestHandler):
                 item.item_link = item_link
                 item.item_description_strip = item_description_strip
                 item.item_price = item_price
+                item_key = item.put()
                 
                 document = search.Document(
-                    doc_id=item_link,
+                    doc_id=str(item_key),
                     fields=[search.TextField(name='item_title', value=item_title),
-                            search.TextField(name='item_description_strip', value=item_description_strip),
+                            search.TextField(name='item_link', value=item_link),
                             search.TextField(name='item_price', value=item_price, language='zh')],
                     language='zh')
                 
                 try:
-                    index = search.Index(name="_INDEX_NAME_ALL")
+                    index = search.Index(name=_INDEX_NAME_ALL)
                     index.put(document)
                 except search.Error:
                     logging.exception('Index Put failed')
                     
-                item.put()
             self.response.out.write('<br><hr>')
-            
+    # def __unicode__(self):
+#       return unicode(self.item_key)
+      
 class KeywordSearchHandler(webapp.RequestHandler):
 # https://developers.google.com/appengine/docs/python/apis
 # http://www.keakon.net/2012/05/09/GAE%E5%B7%B2%E6%94%AF%E6%8C%81%E4%B8%AD%E6%96%87%E5%85%A8%E6%96%87%E6%90%9C%E7%B4%A2
@@ -114,29 +118,110 @@ class KeywordSearchHandler(webapp.RequestHandler):
         k1 = self.request.get("k1")
         k2 = self.request.get("k2")
         k3 = self.request.get("k3")
+        is_union = self.request.get("is_union").lower()
+        
+        global _IS_DEBUG
+        _IS_DEBUG = self.request.get("is_debug").lower()
+#         self.response.out.write('is_debug: '+_IS_DEBUG+'<br>')
+    
+        if _IS_DEBUG == 'true':
+            self.response.out.write('k1: '+k1+'<br>')
+            self.response.out.write('k2: '+k2+'<br>')
+            self.response.out.write('k3: '+k3+'<br>')
+            self.response.out.write('is_union: '+is_union+'<br>')
+            self.response.out.write('_IS_DEBUG: '+_IS_DEBUG+'<br>')
+            self.response.out.write('<br><hr>')
+        
+        if not is_union:
+            print "is_union is empty"
+            self.response.out.write('Please set is_union is true or false')
+        else:
+            query_string = self.generate_query_string(k1, k2, k3, is_union)
+            self.search_by_qstring(query_string)
+
+    def generate_query_string(self, k1, k2, k3, is_union):
+        query_string = "";
+        if is_union == 'false':
+            query_operator = " AND "
+        else:
+            query_operator = " OR "
+        
+        try:
+            if k1 and k2 and k3:
+                query_string = k1+query_operator+k2+query_operator+k3
+            elif k1 and k2:
+                query_string = k1+query_operator+k2
+            elif k1:
+                query_string = k1
+            
+            return query_string
+        except:
+            self.response.out.write('generate_query_string error')
+            print('generate_query_string error')
+        
+    def search_by_qstring(self, query_string):
+        global _IS_DEBUG 
+            
+        try:
+            index = search.Index(name=_INDEX_NAME_ALL)
+            if _IS_DEBUG == 'true':
+                self.response.out.write('query_string: ' + query_string)
+                self.response.out.write('<br><hr>')
+            search_results = index.search(query_string)
+            search_results_json={}
+            search_results_items=[]
+            search_results_items_single={}
+            search_results_json['list']={}
+            
+            if search_results.number_found:
+                for doc in search_results:
+                    search_results_items_single['key'] = doc.doc_id
+                    search_results_items_single['item_title'] = doc.field('item_title').value
+                    search_results_items_single['item_link'] = doc.field('item_link').value
+                    search_results_items_single['item_price'] = doc.field('item_price').value
+                    search_results_items.append(search_results_items_single)
+                    
+                    if _IS_DEBUG == 'true':
+                        self.response.out.write('doc_id: '+doc.doc_id+'<br>')
+                        self.response.out.write('item_title: '+doc.field('item_title').value+'<br>')
+                        self.response.out.write('item_link: '+doc.field('item_link').value+'<br>')
+                        self.response.out.write('item_price: '+doc.field('item_price').value+'<br>')
+                        self.response.out.write('<br><hr>')
+            else:
+                self.response.out.write('0 result')
+            
+            if _IS_DEBUG != 'true':
+                search_results_json['list']['Item'] = search_results_items
+                self.response.out.write( json.dumps(search_results_json) )
+
+        except search.Error:
+            self.response.out.write('search.Error at KeywordSearchHandler'+search.Error )
+
+#TODO Index refresh mechanism
+class SearchIndexRefreshHandler(webapp.RequestHandler):
+    def get(self):
+        k1 = self.request.get("k1")
+        k2 = self.request.get("k2")
+        k3 = self.request.get("k3")
+        is_union = self.request.get("is_union")
         
         self.response.out.write('k1: '+k1+'<br>')
         self.response.out.write('k2: '+k2+'<br>')
         self.response.out.write('k3: '+k3+'<br>')
+        self.response.out.write('is_union: '+is_union+'<br>')
         self.response.out.write('<br><hr>')
         
-        try:
-            index = search.Index(name="_INDEX_NAME_ALL")
-            search_results = index.search(k1)
-            if search_results.number_found:
-                for doc in search_results:
-                    self.response.out.write('doc_id: '+doc.doc_id+'<br>')
-                    self.response.out.write('item_title: '+doc.field('item_title').value+'<br>')
-#                     self.response.out.write( doc.fields ) )
-                    self.response.out.write('<br><hr>')
-            else:
-                self.response.out.write('0 result')
-        except search.Error:
-            self.response.out.write('search.Error at KeywordSearchHandler')
-
+        if not is_union:
+            print "is_union is empty"
+            self.response.out.write('Please set is_union is true or false')
+        else:
+            query_string = self.generate_query_string(k1, k2, k3, is_union)
+            self.search_by_qstring(query_string)
+  
 application = webapp.WSGIApplication([
     ('/', MainHandler),
-    ('/keyword', KeywordSearchHandler),
+    ('/search', KeywordSearchHandler),
+    ('/search_index_refresh', SearchIndexRefreshHandler),
     ('/rest/.*', rest.Dispatcher)
 ], debug=True)
 
